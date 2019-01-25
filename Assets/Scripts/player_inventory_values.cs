a5e8a7752e0c3960ad11613cba2846f8249ba957
DavidK <dkerr3@scu.edu>
2019-01-24 16:27:03 -0800

    First Iteration of UI

---
+++ Assets/Scripts/player_inventory_values.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_inventory_values : MonoBehaviour {
+
+    public int MoneyReserve = 1000;
+    public int[] AmountOfPlants = { 0, 0, 0 };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
