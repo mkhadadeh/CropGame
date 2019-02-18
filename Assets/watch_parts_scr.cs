cf8412a9ec8c00d914fc468f9945dbb24296eba9
Mohammed Khadadeh <mkhadadeh@scu.edu>
2019-02-18 12:50:35 -0800

    Started editing watch beam

---
+++ Assets/watch_parts_scr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class watch_parts_scr : MonoBehaviour {
+    public Transform[] points;
+    public LineRenderer[] line_renderers;
+    Vector3[] positions;
+	// Use this for initialization
+	void Start () {
+        positions = new Vector3[8];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < 8; i++)
+        {
+            positions[i] = points[i].position;
+        }
+        Debug.Log(positions);
+        line_renderers[0].SetPositions(positions);
+	}
+}
