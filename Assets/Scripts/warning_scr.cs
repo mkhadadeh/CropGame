fc31cbe24fbdb9c3b664c29f066a0691c5d99854
Mohammed Khadadeh <mkhadadeh@scu.edu>
2019-02-18 14:37:23 -0800

    Added Warning System Haptics

---
+++ Assets/Scripts/warning_scr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warning_scr : MonoBehaviour {
+    public GameObject warningIcon;
+    private void Update()
+    {
+        byte[] shake = { 255 };
+        OVRHaptics.Channels[0].Preempt(new OVRHapticsClip(shake, 1));
+    }
+
+    private void OnEnable()
+    {
+        warningIcon.SetActive(true);
+    }
+    private void OnDisable()
+    {
+        warningIcon.SetActive(false);
+    }
+}
