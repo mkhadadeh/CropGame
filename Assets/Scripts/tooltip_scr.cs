607b51cd9402ff6cf83b35b17d7be6dfcba8614f
Hagrid <scu.imaginarium@gmail.com>
2019-02-28 11:31:17 -0800

    Tooltips work perfectly

--- Assets/Scripts/tooltip_scr.cs
+++ Assets/Scripts/tooltip_scr.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class tooltip_scr : MonoBehaviour {
     public Transform pointer;
     public Transform playerEye;
+    public Transform[] tooltipAnchors;
+    public Transform[] riverTooltips;
     int show;
 	// Use this for initialization
 	void Start () {
@@ -30,6 +32,13 @@ public class tooltip_scr : MonoBehaviour {
         {
             transform.GetChild(show).gameObject.SetActive(true);
         }
+        riverTooltips[0].position = tooltipAnchors[0].position;
+        Vector3 eulerRotation = new Vector3(tooltipAnchors[0].eulerAngles.x, tooltipAnchors[0].eulerAngles.y, tooltipAnchors[0].eulerAngles.z);
+        riverTooltips[0].rotation = Quaternion.Euler(eulerRotation);
+
+        riverTooltips[1].position = tooltipAnchors[1].position;
+        Vector3 eulerRotation2 = new Vector3(tooltipAnchors[1].eulerAngles.x, tooltipAnchors[1].eulerAngles.y, tooltipAnchors[1].eulerAngles.z);
+        riverTooltips[1].rotation = Quaternion.Euler(eulerRotation2);
     }
 
     public void ShowTip(int i)
