e5200f88e2a9f89a678b93c10d88ac1953216eb6
Hagrid <scu.imaginarium@gmail.com>
2019-03-02 14:06:21 -0800

    Menu Fix

--- Assets/Scripts/main_menu_scr.cs
+++ Assets/Scripts/main_menu_scr.cs
@@ -6,32 +6,24 @@ using UnityEngine.SceneManagement;
 public class main_menu_scr : MonoBehaviour {
 
     public AudioSource intro;
-    bool play;
 	// Use this for initialization
 	void Start () {
-        play = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-        if(play && !intro.isPlaying)
-        {
-            play = false;
-            SceneManager.LoadScene("MainScene");
-        }
+        
 	}
 
 	public void PlayGame() {
-        if (play == false)
-        {
-            intro.Play();
-            play = true;
-        }
-        else
-        {
-            intro.Stop();
-        }
-	}
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Hear()
+    {
+        intro.Play();
+    }
+
     public void QuitGame()
     {
         Application.Quit();
